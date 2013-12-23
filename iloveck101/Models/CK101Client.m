0b5252992f7d2fb1eb09cdb95187a1c7d6510282
Francis Chong <francis@ignition.hk>
2013-12-24 01:56:14 +0800

    set default headers for HTTPClient

--- iloveck101/Models/CK101Client.m
+++ iloveck101/Models/CK101Client.m
@@ -9,6 +9,7 @@
 #import "CK101Client.h"
 
 static NSString * const AFCK101ClientBaseURLString = @"http://ck101.com/";
+static NSString * const AFCK101ClientUserAgent = @"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36'}";
 
 @implementation CK101Client
 
@@ -19,6 +20,10 @@ static NSString * const AFCK101ClientBaseURLString = @"http://ck101.com/";
         _sharedClient = [[CK101Client alloc] initWithBaseURL:[NSURL URLWithString:AFCK101ClientBaseURLString]];
         [_sharedClient setSecurityPolicy:[AFSecurityPolicy policyWithPinningMode:AFSSLPinningModePublicKey]];
         [_sharedClient setResponseSerializer:[AFHTTPResponseSerializer serializer]];
+
+        AFHTTPRequestSerializer* reqSerializer = [AFHTTPRequestSerializer serializer];
+        [reqSerializer setValue:AFCK101ClientUserAgent forHTTPHeaderField:@"User-Agent"];
+        [_sharedClient setRequestSerializer:reqSerializer];
     });
     return _sharedClient;
 }
